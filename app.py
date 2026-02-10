6e35578f706ebc628c2ff8b6ec10e7a076a6fe7e
ThiagoPaulim <thiagopaulim2001@gmail.com>
2026-02-10 15:35:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -252,4 +252,4 @@ def start_test(payload: dict):
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
